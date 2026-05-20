cfe712e05aa206f3f5aa457ae517cf421614b7c0
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-05-20 18:10:44 +0530

    Config updated

---
+++ Configs/cmus/lib.pl
@@ -0,0 +1,5 @@
+/home/blackspark/Music/Afreen Afreen - Rahat Fateh Ali Khan.mp3
+/home/blackspark/Music/Ishqa Ve - Zeeshan Ali.flac
+/home/blackspark/Music/Hiten - Tere_Utto_Sab_Vaar_diya_INTO_YOU_Hiten_Latest_Punjabi_Romantic_Songs.opus
+/home/blackspark/Music/Baarish Ke Aane Se - Tony Kakkar, Shreya Ghoshal, Prince Dubey.flac
+/home/blackspark/Music/Vinland Saga.m4a
